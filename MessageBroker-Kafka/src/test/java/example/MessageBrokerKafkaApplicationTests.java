8c89c7202f95486594f437ef01b63f41ab1cca08
Maria Mcnally <cph-mm620@cphbusiness.dk>
2023-11-26 15:42:05 +0100

    removed and cleaned

--- MessageBroker-Kafka/src/test/java/example/MessageBrokerKafkaApplicationTests.java
+++ MessageBroker-Kafka/src/test/java/example/MessageBrokerKafkaApplicationTests.java
@@ -1,11 +1,5 @@
 package example;
 
-<<<<<<<< HEAD:MessageBroker-Kafka/src/test/java/com/MessageBrokerKafkaApplicationTests.java
-package com.example;
-========
-package com.messagebrokerkafka;
->>>>>>>> d4bd8eba31c2c2dc8f8ee60675c25473a9062027:MessageBroker-Kafka/src/test/java/com/messagebrokerkafka/MessageBrokerKafkaApplicationTests.java
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 

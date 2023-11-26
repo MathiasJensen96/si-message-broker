98603c6e7fb6bfeedda8e65134014ecf5f0e03f8
Maria Mcnally <cph-mm620@cphbusiness.dk>
2023-11-26 12:56:14 +0100

    commit startcode

---
+++ MessageBroker-Kafka/src/main/java/com/example/messagebrokerkafka/MessageBrokerKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.messagebrokerkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBrokerKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageBrokerKafkaApplication.class, args);
+    }
+
+}
---
+++ MessageBroker-Kafka/src/test/java/com/example/messagebrokerkafka/MessageBrokerKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagebrokerkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBrokerKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
